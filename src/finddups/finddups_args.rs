33cfd8a0d8e42f2d04ac7e605738b0275ca8db26
George Madrid <gmadrid@gmail.com>
2016-10-03 16:42:25 -0400

    Adding args for finddups command.

--- src/finddups/finddups_args.rs
+++ src/finddups/finddups_args.rs
@@ -1,6 +1,13 @@
-use clap::{App, SubCommand};
+use clap::{App, Arg, SubCommand};
 
+static ACTION: &'static str = "action";
+static DIR: &'static str = "DIR";
 static SUBCOMMAND: &'static str = "finddups";
+
+const PREVIEW: &'static str = "preview";
+const PRINT: &'static str = "print";
+const QUIET: &'static str = "quiet";
+const ACTION_NAMES: [&'static str; 3] = [PREVIEW, PRINT, QUIET];
 
 // #[derive(Debug)]
 // pub struct FinddupsArgs {
@@ -13,7 +20,17 @@ pub trait AddFinddupsSubcommand<'a, 'b> {
 
 impl<'a, 'b> AddFinddupsSubcommand<'a, 'b> for App<'a, 'b> {
   fn add_finddups_subcommand(self) -> App<'a, 'b> {
-    self.subcommand(SubCommand::with_name(SUBCOMMAND))
+    self.subcommand(SubCommand::with_name(SUBCOMMAND)
       .about("find exact duplicates across a list of directories")
+      .arg(Arg::with_name(DIR)
+        .help("Directories to crawl")
+        .index(1)
+        .multiple(true)
+        .required(true))
+      .arg(Arg::with_name(ACTION)
+        .long(ACTION)
+        .help("The action to take with matching groups")
+        .possible_values(&ACTION_NAMES)
+        .default_value(PRINT)))
   }
 }
